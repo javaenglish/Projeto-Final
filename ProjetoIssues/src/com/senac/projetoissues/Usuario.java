87d3f67baeee08539d59ce612e90e2c848757669
Tarcisio Monteiro <rafandai@gmail.com>
2014-06-09 08:54:25 -0300

    Teste de Username, passou

--- ProjetoIssues/src/com/senac/projetoissues/Usuario.java
+++ ProjetoIssues/src/com/senac/projetoissues/Usuario.java
@@ -2,4 +2,20 @@ package com.senac.projetoissues;
 
 public class Usuario {
 
+	private String nome;
+	private String username;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 }
