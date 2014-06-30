8ed20de996f171fde15437b8b525580f2ae153f8
Tarcisio Monteiro <rafandai@gmail.com>
2014-06-30 09:06:34 -0300

    Tentativa de criacao de Lista

--- ProjetoIssues/src/com/senac/projetoissues/lista/Lista.java
+++ ProjetoIssues/src/com/senac/projetoissues/lista/Lista.java
@@ -1,15 +1,69 @@
 package com.senac.projetoissues.lista;
 
-public class Lista {
+public class Lista <T> {
 
-	private int tail;
-	public void adicionar(int i) {
+	private Nodo <T> tail;
+	private Nodo <T> head;
 	
+	
+	private boolean Vazia() {
+		if(head == null){
+			return true;
+		}else{
+			return false;
+		}		
+	}
+	
+	
+
+	public void adicionar(T valor) {
+		Nodo<T> novo = new Nodo<T>(valor);
+		if ( Vazia() ) {
+			this.head = novo;
+			this.tail = novo;
+		}
+		else {
+			Nodo<T> nodo = procuraProximo(valor);
+
+			if ( nodo != null ) {
+				Nodo<T> nodoAnte = nodo.getAnterior();
+				Nodo<T> nodoProx = nodo;
+
+				novo.setProximo(nodoProx);
+				novo.setAnterior(nodoAnte);
+
+				if ( nodoAnte != null )
+					nodoAnte.setProximo(novo);
+				else
+					this.head = novo;
+
+				nodoProx.setAnterior(novo);	
+			}
+			else {
+				Nodo<T> nodoAnte = tail;
+				Nodo<T> nodoProx = null;
+
+				novo.setProximo(nodoProx);
+				novo.setAnterior(nodoAnte);
+
+				tail.setProximo(novo);
+				tail = novo;
+			}
+
+		}
+	}
+	private Nodo<T> procuraProximo(T valor) {
 		
+				return null;
+	}
+	
+	public Nodo<T> getTail() {
+		return tail;
 	}
 
-	public int getTail() {
-		return tail();		
+	public Nodo getHead() {
+
+	return head;
 	}
 
 }
--- ProjetoIssues/src/com/senac/projetoissues/teste/TesteLista.java
+++ ProjetoIssues/src/com/senac/projetoissues/teste/TesteLista.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.senac.projetoissues.lista.Lista;
+
 import static org.mockito.Mockito.*;
 
 public class TesteLista {
